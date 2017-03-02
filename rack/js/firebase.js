a8abf7bb9a18425d0aee7c88b48b186f2e498f9b
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-03-02 15:15:41 -0700

    Rack App
    Aplicación como alternativa a google spaces

---
+++ rack/js/firebase.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDrtoAdJBCu3gO1E64F1ZHpenTrzQ57rVg",
+    authDomain: "raite-49ffd.firebaseapp.com",
+    databaseURL: "https://raite-49ffd.firebaseio.com",
+    storageBucket: "raite-49ffd.appspot.com",
+    messagingSenderId: "903577680561"
+};
+firebase.initializeApp(config);
