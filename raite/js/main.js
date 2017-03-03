cc827ae1647954a9f4b7f0d8281c7f98289064ca
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-03-02 22:09:10 -0700

    User log

--- raite/js/main.js
+++ raite/js/main.js
@@ -1,18 +1,26 @@
 jQuery(document).ready(function($) {
-    var userName = firebase.auth().currentUser.displayName;
-
-    console.log(userName);
-
-    if (user != null) {
-        console.log(user);
-        name = user.displayName;
-        email = user.email;
-        photoUrl = user.photoURL;
-        uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-        // this value to authenticate with your backend server, if
-        // you have one. Use User.getToken() instead.
-        alert(user.displayName)
-        console.log(user.displayName);
-        $('#user_login').text(user.displayName);
-    }
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            console.log(user);
+        } else {
+            // No user is signed in.
+        }
+    });
+    // var userName = firebase.auth().currentUser.displayName;
+    //
+    // console.log(userName);
+    //
+    // if (user != null) {
+    //     console.log(user);
+    //     name = user.displayName;
+    //     email = user.email;
+    //     photoUrl = user.photoURL;
+    //     uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+    //     // this value to authenticate with your backend server, if
+    //     // you have one. Use User.getToken() instead.
+    //     alert(user.displayName)
+    //     console.log(user.displayName);
+    //     $('#user_login').text(user.displayName);
+    // }
 });
