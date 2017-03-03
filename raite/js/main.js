2ec53c3678633a3c7bd4ced16fae963593bfc119
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-03-02 22:49:55 -0700

    condicion

--- raite/js/main.js
+++ raite/js/main.js
@@ -2,11 +2,11 @@ jQuery(document).ready(function($) {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
-            if( $('#user_login').text() == 'Iniciar sesión'){
-                 location.reload(true);
-            }
              $('#user_login').text(user.displayName);
              $('#logout').text("Cerrar sesión");
+             if( $('#user_login').text() == 'Iniciar sesión'){
+                  location.reload(true);
+             }
         } else {
             // No user is signed in.
             $('#logout').text("");
