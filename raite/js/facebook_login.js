5b7666aa781a2c453333b443cbd6581c546dc650
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-03-02 21:51:03 -0700

    Revisar login al cargar

--- raite/js/facebook_login.js
+++ raite/js/facebook_login.js
@@ -20,29 +20,31 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
     // ...
 });
 
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+$(document).ready(function(){
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+})
 
 function logout() {
     firebase.auth().signOut().then(function() {
   // Sign-out successful.
 }, function(error) {
   // An error happened.
-}); 
+});
 }
