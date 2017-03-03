c37693d2f1033ef459d3dad18be5dff9561ebdae
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-03-02 22:11:04 -0700

    Login button

--- raite/js/main.js
+++ raite/js/main.js
@@ -8,20 +8,4 @@ jQuery(document).ready(function($) {
             // No user is signed in.
         }
     });
-    // var userName = firebase.auth().currentUser.displayName;
-    //
-    // console.log(userName);
-    //
-    // if (user != null) {
-    //     console.log(user);
-    //     name = user.displayName;
-    //     email = user.email;
-    //     photoUrl = user.photoURL;
-    //     uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-    //     // this value to authenticate with your backend server, if
-    //     // you have one. Use User.getToken() instead.
-    //     alert(user.displayName)
-    //     console.log(user.displayName);
-    //     $('#user_login').text(user.displayName);
-    // }
 });
