3c807bffc1b839d900be6fee2b1990f0d87af364
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-03-02 23:49:26 -0700

    uid

--- raite/js/main.js
+++ raite/js/main.js
@@ -9,6 +9,7 @@ jQuery(document).ready(function($) {
         if (user) {
             // User is signed in.
             $('#user_login').text(user.displayName);
+            $('#uid').text(user.uid);
             $('#logout').text("Cerrar sesión");
         } else {
             // No user is signed in.
@@ -16,3 +17,25 @@ jQuery(document).ready(function($) {
         }
     });
 });
+
+function guardarViaje(uid, car_type, picture, title, body) {
+  // A post entry.
+  var postData = {
+    author: username,
+    uid: uid,
+    body: body,
+    title: title,
+    starCount: 0,
+    authorPic: picture
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('travels').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/travels/' + newPostKey] = postData;
+  updates['/user-travels/' + uid + '/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
