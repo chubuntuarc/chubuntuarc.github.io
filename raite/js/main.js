5f3edbbdb867aa599f4ad936c400a54fcd2acba2
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-03-02 21:54:37 -0700

    Test alert

--- raite/js/main.js
+++ raite/js/main.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function($) {
-    alert('Test')
     var user = firebase.auth().currentUser;
 
     if (user != null) {
@@ -10,7 +9,7 @@ jQuery(document).ready(function($) {
         uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
         // this value to authenticate with your backend server, if
         // you have one. Use User.getToken() instead.
+        console.log(user.displayName);
         $('#user_login').text(user.displayName);
     }
-    $(".button-collapse").sideNav()
 });
